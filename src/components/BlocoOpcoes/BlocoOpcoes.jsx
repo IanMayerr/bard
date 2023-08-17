9551c5f13424a6f0856f7359c950d2c685ff2246
Ian Mayer <128386491+IanMayerr@users.noreply.github.com>
2023-08-17 13:31:44 -0300

    Update BlocoOpcoes.jsx

--- src/components/BlocoOpcoes/BlocoOpcoes.jsx
+++ src/components/BlocoOpcoes/BlocoOpcoes.jsx
@@ -3,23 +3,43 @@ import fundoLogin from "../../assets/fundoLogin.jpg"
 
 
 export const ContainerFundo = styled.div`
-    align-items: center;
-    justify-content: left;
+    display: flex;
+    flex-direction: column;
+    gap: 2vh;
     background-color: #2B2B2B;
     border-radius: 27px;
-    width: 35vh;
-    height: 35vh;
+    width: 95vw;
+    height: 50vh;
+    margin-top: 60px;
+    margin-left: 8px;
+    padding-top: 25px;
+    align-items: center;
 `
 
 export const ParteFuncional = styled.div`
+    display:flex;
     align-items: center;
-    justify-items: center;
     background-image: url(${fundoLogin});
     height: 50vh;
     width: 50vh;
-    padding: 50px;
 `
 
 export const Fonte = styled.h1`
     color: white;
+`
+
+export const Titulo = styled.h2`
+    text-align: center;
+    align-items: center;
+    color: white;
+    font-size: 20px;
+`
+
+export const ContainerLinha = styled.div`
+    width: 20vw;
+    height: 0.44vh;
+    background-color: #827397;
+    align-items: center;
+    margin-bottom: 15px;
+    
 `
