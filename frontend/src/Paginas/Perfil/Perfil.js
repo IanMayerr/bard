05985dcdeffa0c540c95873b289e34effae89139
Ian Mayer <ianbeckmayer@gmail.com>
2023-10-18 15:09:01 -0300

    Tela de Perfil pronta

--- frontend/src/Paginas/Perfil/Perfil.js
+++ frontend/src/Paginas/Perfil/Perfil.js
@@ -1,5 +1,5 @@
 import setaVoltar from "../../assets/setaVoltar.svg"
-import { Banner, Nome, Arroba, Cidade, Fundo, ParteDeCima, Img, Status, ParteDoMeio, Biografia, TextoBiografia } from "./Perfil.jsx"
+import { Banner, Nome, Arroba, Cidade, Fundo, ParteDeCima, Img, Status, ParteDoMeio, Biografia, TextoBiografia, Img2 } from "./Perfil.jsx"
 import fotoPerfil from "../../assets/fotoPerfil.svg"
 import iconeLocalizacao from "../../assets/iconeLocalizacao.svg"
 import Card from "../../components/Card/Card.js"
@@ -15,18 +15,17 @@ function Perfil() {
                 <ParteDeCima>
                     <Img src={fotoPerfil} alt='' />
 
-                        <Nome>Ian Mayer</Nome>
-                        <Arroba>@ianziito</Arroba>
-                        <Status>Status: Músico(a) Amadador(a)</Status>
-                        <img src={iconeLocalizacao} alt='' />
-                        <Cidade>São Leopoldo</Cidade>
+                    <Nome>Ian Mayer</Nome>
+                    <Arroba>@ianziito</Arroba>
+                    <Status>Status: Músico(a) Amadador(a)</Status>
+                    <Cidade>
+                        <Img2 src={iconeLocalizacao} alt='' />
+                        São Leopoldo
+                    </Cidade>
                 </ParteDeCima>
 
-                <div>
-                </div>
-
                 <ParteDoMeio>
-                    <Biografia>Biografia:</Biografia>
+                    <Biografia>Bio:</Biografia>
                     <TextoBiografia>Músico iniciante. 17y. Gosto de tocar sertanejo e MPB. Disponível para fazer um som com qualquer um, chama na DM!</TextoBiografia>
                 </ParteDoMeio>
 
--- frontend/src/Paginas/Perfil/Perfil.jsx
+++ frontend/src/Paginas/Perfil/Perfil.jsx
@@ -15,57 +15,88 @@ export const Fundo = styled.div`
 `
 
 export const Nome = styled.h1`
-    font-size: 19px;
+    font-size: 18px;
     color: #D8B9C3;
     grid-column: 2/2;
     grid-row: 1/1;
+    padding-left: 5px;
+    text-shadow: 1.5px 1.5px 1.5px black;
+
 `
 
 export const Arroba = styled.h1`
-    font-size: 16px;
+    font-size: 14px;
     color: #D8B9C3;
     grid-column: 2/2;
     grid-row: 2/2;
+    padding-left: 5px;
+    padding-bottom: 8px;
+    text-shadow: 1.5px 1.5px 1.5px black;
+
 `
 
 export const Status = styled.h1`
-    font-size: 14px;
+    font-size: 12px;
     color: #D8B9C3;
     opacity: 50%;
-    grid-column: 3/3;
-    grid-row: 1/1;
+    grid-column: 3/4;
+    padding-right:5px ;
+    grid-row: 1/2;
+    text-align: end;
+    text-shadow: 1.5px 1.5px 1.5px black;
 `
 
+
 export const Cidade = styled.h1`
-    font-size: 16px;
+    font-size: 85%;
     color: #D8B9C3;
     font-weight: bold;
+    display: flex;
+    flex: column;
+    grid-column: 2/4;
+    grid-row: 3/3;
+    text-shadow: 1.5px 1.5px 1.5px black;
+
 `
 
 export const Biografia = styled.h1`
     font-size: 16px;
     color: #D8B9C3;
     opacity: 60%;
+    text-shadow: 2px 2px 2px black;
+
 `
 
 export const TextoBiografia = styled.div`
+    color: #D8B9C3;
+    font-size: 14px;
+    /* text-shadow: 0.5px 0.5px 0.5px #67595E; */
 
 `
 
 export const ParteDeCima = styled.div`
     display: grid;
-    padding: 20px;
-    grid-template-columns: 33% 33% 33%;
-    grid-template-rows: 33% 33% 33%;
-    /* border: 1px solid red; */
+    margin: 2vw 2vw 0 2vw;
+    grid-template-columns: 33% 40% 27%;
+    grid-template-rows: 29% 34% 25%;
+    height: 100%;
 `
 
 export const Img = styled.img`
     grid-column: 1/1;
     grid-row: 1/3;
+    height: 140%;
+    width: 30vw;
+    
 `
 
+export const Img2 = styled.img`
+    height: 25px;
+    width: 10vw;
+`
+
+
 export const ParteDoMeio = styled.div`
-    padding-left: 25px;
-    padding-right: 15px;
+    padding-left: 20px;
+    padding-right: 10px;
 `
